ae037cdbef4dba806ba5e61a5b333343c5aad180
Ramkishan <ramkishankamble@gmail.com>
2025-02-26 15:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function firstWord(s) {
   // your code here
-	if (str.length === 0) return ""; // Handle empty string case
+	str = str.trim();
 
-    let index = str.indexOf(" ");
-    
-    return index === -1 ? str : str.slice(0, index); 
+	let index = str.indexof(" ");
+
+	return index === -1 ? str : str.slice(0,index);
+	
 }
 
 // Do not change the code below
